80df7ae582c6ba98cf5650af24cbe6422ad73cf4
r16fourarm <qadrian@students.telkomuniversity.ac.id>
2015-03-15 13:19:22 +0700

    holaholapoom
    added some table class n interface

---
+++ src/tubes/MataKuliah.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes;
+
+/**
+ *
+ * @author R16
+ */
+public class MataKuliah {
+    private String nama;
+    private String kdMk;
+
+    public MataKuliah(String nama, String kdMk) {
+        this.nama = nama;
+        this.kdMk = kdMk;
+    }
+
+    public String getKdMk() {
+        return kdMk;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+    
+}
---
+++ src/tubes/OlahDataDosen.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tubes;
+
+/**
+ *
+ * @author R16
+ */
+import java.util.ArrayList;
+public class OlahDataDosen implements TabelOlahData{
+    private ArrayList<Dosen> dataDosen;
+
+    public OlahDataDosen() {
+        this.dataDosen = new ArrayList<Dosen>();  
+    }
+    public void addDosen(Dosen d){
+      dataDosen.add(d);
+    }
+    public void removeDosen(String nama){
+        for (Dosen d : dataDosen){
+            if (d.getNama()== nama){
+                dataDosen.remove(d);
+            }
+            else
+            {
+                System.out.println("Data tidak ditemukan");
+                break;
+            }    
+        }
+    }
+    public void viewAll(){
+        for(Dosen d: dataDosen){
+            System.out.println(d.getNama());
+        }
+    }
+}
